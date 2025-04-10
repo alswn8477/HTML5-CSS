c1e6ed357edece52084229a606a10783f11f06dd
alswn8477 <alswn8477@naver.com>
2025-04-10 17:41:10 +0900

    first commit

---
+++ HTML/_이민주포폴/ACEBED/script.js
@@ -0,0 +1,14 @@
+// script.js
+function loadAlert() {
+  alert("Test!");
+}
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
