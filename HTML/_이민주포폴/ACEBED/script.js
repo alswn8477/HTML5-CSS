b4e11dfdf88e13e4a88cd916ce4bd05fc19d5d52
alswn8477 <alswn8477@naver.com>
2025-04-11 17:38:31 +0900

    first commit

--- HTML/_이민주포폴/ACEBED/script.js
+++ HTML/_이민주포폴/ACEBED/script.js
@@ -3,12 +3,45 @@ function loadAlert() {
   alert("Test!");
 }
 
+// * con2 *
+
+let a = document.querySelectorAll(".con2 .down .left a");
+let lir = document.querySelectorAll(".con2 .down .right li");
+
+a[0].classList.add("on");
+lir[0].classList.add("on");
+
+function myr() {
+  a.forEach(function (v, k) {
+    v.classList.remove("on");
+    lir.forEach(function (v, k) {
+      v.classList.remove("on");
+    });
+  });
+}
+
+a.forEach(function (v, k) {
+  v.onclick = function (event) {
+    event.preventDefault();
+    myr();
+    a[k].classList.add("on");
+    lir[k].classList.add("on");
+  };
+});
+
+// * con3 *
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: "auto",
-  centeredSlides: true,
+  slidesPerView: 1,
   spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
